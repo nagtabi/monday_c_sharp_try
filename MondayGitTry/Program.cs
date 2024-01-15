03fab0e4ab73dd786a084ac89f54b42d7692a0b9
dhfear <dhfear@hotmail.com>
2024-01-15 18:27:01 +0100

    solution

--- MondayGitTry/Program.cs
+++ MondayGitTry/Program.cs
@@ -17,6 +17,21 @@ namespace MondayGitTry
 
         public static String Welcome(string name)
         {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Kérlek add meg a neved!");
+            string neved = Console.ReadLine();
+            if (now.Hour >= 4 && now.Hour < 11) 
+            {
+                Console.WriteLine("Jó reggelt " + neved);
+            }
+            else if (now.Hour >= 11 && now.Hour < 18) 
+            {
+                Console.WriteLine("Jó napot " + neved);
+            }
+            else
+            {
+                Console.WriteLine("Jó szerencsét " + neved);
+            }
             return "";
             //reggel 4-11 Jo reggelt {name}
             //11 - 18 Jo napot {name}
