4fc32ff401e18030a2462ef46530bca5baa6f24c
nagtabi <nagy.gabor70@gmail.com>
2024-01-15 18:01:19 +0100

    firts commit

---
+++ MondayGitTry/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MondayGitTry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Welcome("...");
+            Console.WriteLine(text);
+            Console.ReadLine(); 
+        }
+
+        public static String Welcome(string name)
+        {
+            return "";
+            //reggel 4-11 Jo reggelt {name}
+            //11 - 18 Jo napot {name}
+            // Jo szerencset {name}
+        }
+    }
+}
